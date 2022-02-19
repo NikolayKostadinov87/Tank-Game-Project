4ff06a2f86b550e8389fadca380703be81e5933f
Николай Костадинов <79536611+NikolayKostadinov87@users.noreply.github.com>
2022-02-19 13:57:11 +0200

    Update greekScenario.js

--- Scripts/greekScenario.js
+++ Scripts/greekScenario.js
@@ -1280,7 +1280,7 @@ function greekScenario() {
         })
   
         frames++;
-        //SpawnSuicideEnemiesFunction();
+        SpawnSuicideEnemiesFunction();
         FirstAlphaSniperEnemyFunction();
         SecondAlphaSniperEnemyFunction();
         ThirdAlphaSniperEnemyFunction();
